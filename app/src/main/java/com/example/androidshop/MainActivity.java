3810ce6d0bb6af1d08e6696a214ab6db4cba4c74
patryk.mysiorski <patryk.mysiorski@gmail.com>
2021-06-13 00:13:09 +0200

    add image slider with discounts

--- app/src/main/java/com/example/androidshop/MainActivity.java
+++ app/src/main/java/com/example/androidshop/MainActivity.java
@@ -5,19 +5,34 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        prepareImageSlider();
     }
 
     public void logout(View view) {
         firebaseAuth.signOut();
+    }
+
+    public void prepareImageSlider() {
+        ImageSlider imageSlider = findViewById(R.id.image_slider);
+        List<SlideModel> slideModelList = new ArrayList<>();
+        slideModelList.add(new SlideModel(R.drawable.banner1, getString(R.string.shoesDiscount), ScaleTypes.CENTER_CROP));
+        slideModelList.add(new SlideModel(R.drawable.banner2, getString(R.string.womenDiscount), ScaleTypes.CENTER_CROP));
+        slideModelList.add(new SlideModel(R.drawable.banner3, getString(R.string.christmasDiscount), ScaleTypes.CENTER_CROP));
+        imageSlider.setImageList(slideModelList);
     }
 }
